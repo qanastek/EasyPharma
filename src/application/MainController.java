88d5d079172888d307234f20c526297714ef1caa
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2020-11-30 20:11:11 +0100

    first controller

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,5 +1,17 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class MainController {
+	
+    @FXML
+    private Text content;
+
+    @FXML 
+    private void handleButton1() {
+    	content.setText("saluce hombre!");
+        System.out.println("saluce hombre!");
+    } 
 	
 }
